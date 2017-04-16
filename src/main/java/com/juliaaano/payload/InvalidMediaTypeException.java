a205f69b91de1f99a97f6403821d79bdf6ee9048
juliaaano <juliaaano@gmail.com>
2017-04-16 21:05:02 +1000

    Add javadoc to Payload, PayloadFactory and Exception

--- src/main/java/com/juliaaano/payload/InvalidMediaTypeException.java
+++ src/main/java/com/juliaaano/payload/InvalidMediaTypeException.java
@@ -1,5 +1,10 @@
 package com.juliaaano.payload;
 
+/**
+ * General exception class to report internal errors with the library.
+ *
+ * @author Juliano Boesel Mohr
+ */
 public class InvalidMediaTypeException extends IllegalArgumentException {
 
     private static final long serialVersionUID = 183253650406525751L;
